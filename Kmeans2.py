19be7b6e891a511f30f9e991fed0f67b27b5575d
grupopdicyf <cristmartinez@udec.cl>
2016-05-01 22:05:20 +0000

    CodigoRespaldo

---
+++ Kmeans2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from SimpleCV import Camera, Display, Image
+from sklearn.cluster import MiniBatchKMeans
+import numpy as np
+import cv2
+
+cam = Camera()
+image = cam.getImage().save("imagen.jpg")
+
+print("Ingrese numero de Clusters: ")
+clusters = input()
+
+#image = image.grayscale().save("imagen.jpg")
+
+image = cv2.imread("imagen.jpg")
+
+(h, w) = image.shape[:2]
+
+image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+image = image.reshape((image.shape[0] * image.shape[1], 3))
+clt = MiniBatchKMeans(n_clusters = clusters)
+labels = clt.fit_predict(image)
+quant = clt.cluster_centers_.astype("uint8")[labels]
+
+quant = quant.reshape((h, w, 3))
+image = image.reshape((h, w, 3))
+
+quant = cv2.cvtColor(quant, cv2.COLOR_LAB2BGR)
+image = cv2.cvtColor(image, cv2.COLOR_LAB2BGR)
+
+cv2.imshow("image", np.hstack([image, quant]))
+cv2.waitKey(0)
